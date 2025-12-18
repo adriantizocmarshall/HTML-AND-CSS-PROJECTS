def2067d3c5f043d93d7e515785ef541f5e3b33c
adriantizocmarshall <161913596+adriantizocmarshall@users.noreply.github.com>
2025-12-17 22:10:08 -0500

    Completed Part Three of "Adding JavaScript to an HTML and CSS Website" video series

--- Simple-Recipes/javascript/javascript.js
+++ Simple-Recipes/javascript/javascript.js
@@ -27,3 +27,53 @@ for (var i = 0; i < closeBtn.length; i++) {
         }
     }
 }
+
+//EMAIL VALIDATION
+
+document.getElementById('contactForm').addEventListener('submit', 
+    function (event) {
+        //Overrides the default browser refresh when the submit
+        //button is pressed
+        event.preventDefault();
+
+
+        //Variables to validate that each field is filled out
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const message = document.getElementById('message').value;
+
+        //Email pattern checks for all symbols that would be needed
+        //for an email address such as the @ and . and the text
+        //that would come before, between, and after.
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+        //Use this variable to display messages if fields are filled
+        //out or thank you message
+        const valMsg = document.getElementById('validateMsg');
+
+        if (!firstName || !lastName || !phone || !message) {
+            //Checks if fields have been filled out
+            valMsg.innerHTML = '<p style="color: red;">Please fill out all fields.</p>';
+        } else if (!emailPattern.test(email)) {
+            //Checks if there is a valid email
+            valMsg.innerHTML = '<p style="color: red;">Please enter a valid email address.</p>';
+        } else {
+            //If all fields are filled out correctly display thank you message
+            calMsg.innerHTML = '<p style="color: red;">Thank you for submitting!</p>';
+        }
+
+        const formData = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            message: message,
+            subscribe: document.getElementById('subscription').checked
+        };
+
+        //Displays what the user wrote in the console
+        console.log(JSON.stringify(formData))
+    }
+)
